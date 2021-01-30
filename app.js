921ee856f9adf723bcf564487d2ee1b635d053b8
bedeko66 <bedekovicsl@gmail.com>
2021-01-30 14:37:37 +0000

    random dog pictures app v1

---
+++ app.js
@@ -0,0 +1,67 @@
+async function getUrl() {
+    try {
+        const fetched = await fetch("https://random.dog/woof.json?ref=apilist.fun");
+        let jsonRes =  await fetched.json()
+        let aElem = document.getElementById('link'); 
+        aElem.href = jsonRes.url;
+
+        if ( jsonRes.url.includes(".jpg") || jsonRes.url.includes(".png") || jsonRes.url.includes(".jpeg") || jsonRes.url.includes(".gif") ) {
+            
+            if(document.getElementById('dog-image')) {
+                document.getElementById('dog-image').remove();
+            }
+            if(document.getElementById('dog-video')) {
+                document.getElementById('dog-vid').remove();
+            }
+
+            let img = document.createElement('img');
+            img.src = jsonRes.url;
+            img.id = 'dog-image'
+            document.getElementById('dog-img').append(img);
+            document.getElementById('dog-pic-url').value = jsonRes.url;
+
+        }
+        if ( jsonRes.url.includes(".mp4") ) {
+
+            if(document.getElementById('dog-image')) {
+                document.getElementById('dog-image').remove();
+            }
+
+            if(document.getElementById('dog-video')) {
+                document.getElementById('dog-vid').remove();
+            }
+
+            let videoElem = document.getElementById('dog-vid')
+            
+            videoElem.style.display='block'
+            let vid = document.createElement('source');
+            vid.src = jsonRes.url;
+            vid.type = "video/mp4"
+            vid.id = 'dog-video'
+            videoElem.append(vid);
+            document.getElementById('dog-pic-url').value = jsonRes.url;
+        }
+    }
+    catch (error) {
+        throw new Error(error.message);
+    }
+
+}
+
+async function copyUrl() {
+    try {
+        if(document.getElementById('dog-image')) {
+            let copyText = document.getElementById("dog-image");
+            await navigator.clipboard.writeText(copyText.src);
+        }
+
+        if(document.getElementById('dog-video')) {
+            let copyText = document.getElementById("dog-video");
+            await navigator.clipboard.writeText(copyText.src);
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+
+  }
+
